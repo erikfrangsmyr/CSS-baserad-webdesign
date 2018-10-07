cec2387ee606d159b453757b9416d0146d37e224
erikfrangsmyr <erik.frangsmyr@hotmail.com>
2018-10-07 18:44:24 +0200

    -

---
+++ Projektarbete/script.js
@@ -0,0 +1,9 @@
+
+function toggleMenu() {
+  var x = document.getElementById('dropdown');
+  if (x.style.display === 'flex') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'flex';
+  }
+}
