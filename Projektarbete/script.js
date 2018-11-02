1de8b74985087b8aa67070305ce224f31af7436c
erikfrangsmyr <erik.frangsmyr@hotmail.com>
2018-11-02 22:26:32 +0100

    -

--- Projektarbete/script.js
+++
@@ -1,9 +0,0 @@
-
-function toggleMenu() {
-  var x = document.getElementById('dropdown');
-  if (x.style.display === 'flex') {
-    x.style.display = 'none';
-  } else {
-    x.style.display = 'flex';
-  }
-}
